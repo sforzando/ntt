7c5af4c3b563756987865bb14eadaee8b06d347d
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2018-05-25 19:27:31 +0900

    :+1: Add ipc

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-const Electron = require('electron')
+const { app, BrowserWindow, ipcMain, Electron } = require('electron')
 const HTTP = require('http')
 const NodeStatic = require('node-static')
-
-const App = Electron.app
-const BrowserWindow = Electron.BrowserWindow
 
 const file = new NodeStatic.Server(__dirname + '/public')
 HTTP.createServer((request, response) => {
@@ -21,9 +18,9 @@ function createWindow() {
     acceptFirstMouse: true,
     allowRunningInsecureContent: true,
     alwaysOnTop: true,
-    fullscreen: true,
+    // fullscreen: true,
     frame: false,
-    kiosk: true,
+    // kiosk: true,
     title: 'Numbered Ticket Terminal',
     webSecurity: false
   })
@@ -41,17 +38,28 @@ function createWindow() {
     mainWindow = null
   })
 }
-App.on('ready', () => {
+app.on('ready', () => {
   createWindow()
 })
 
-App.on('activate', () => {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
   }
 })
-App.on('window-all-closed', () => {
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.sender.send('asynchronous-reply', 'pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.returnValue = 'pong'
+})
+
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    App.quit()
+    app.quit()
   }
 })
