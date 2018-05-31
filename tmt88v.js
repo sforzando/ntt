b71d26f3f09308de03bc6f0921e28811055f1c71
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2018-05-31 17:40:35 +0900

    [wip] Add test script for TM-T88V

---
+++ tmt88v.js
@@ -0,0 +1,29 @@
+const escpos = require('escpos')
+const deviceInformation = escpos.USB.findPrinter()[0]
+const device = new escpos.USB(
+  deviceInformation.deviceDescriptor.idVendor,
+  deviceInformation.deviceDescriptor.idProduct
+)
+printer = new escpos.Printer(device, {})
+
+device.open(() => {
+  printer
+    .font('a')
+    .align('ct')
+    .size(2, 2)
+    .text('TEST')
+    .text('utf8のテスト', 'utf8')
+    .text('utf16のテスト', 'utf16')
+    .text('cp437のテスト', 'cp437')
+    .text('cp932のテスト', 'cp932')
+    .text('windows932のテスト', 'windows932')
+    .text('shift_jisのテスト', 'shift_jis')
+    .text('windows932のテスト', 'windows932')
+    .text('euc-jpのテスト', 'euc-jp')
+    .text('asciiのテスト', 'ascii')
+    .text('iso646jpのテスト', 'iso646jp')
+    .text('874のテスト', '874')
+    .control('LF')
+    .cut()
+    .close()
+})
